9ae2bace5cd61c7e9b16aea4bfc705949090e8a1
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2018-01-03 15:58:47 +0800

    support debugger

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#git clone https://github.com/riscv/riscv-openocd.git
+git clone https://github.com/riscv/riscv-fesvr.git
+git clone https://github.com/hchsiao/riscv-isa-sim.git
+git clone https://github.com/hchsiao/riscv-openocd.git
 git clone https://github.com/hchsiao/zero-riscy.git
 git clone https://github.com/hchsiao/openocd_jtag_dpi.git
 git clone http://vlsigit.ee.ncku.edu.tw/hchsiao/zero-buggy.git
