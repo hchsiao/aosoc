5440e9aad7647c90af807e7567c7f0eab97bf199
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2020-05-09 17:58:27 +0800

    change submodule url (moving repo to github)

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ git clone https://github.com/hchsiao/riscv-isa-sim.git
 git clone https://github.com/hchsiao/riscv-openocd.git
 git clone https://github.com/hchsiao/zero-riscy.git
 git clone https://github.com/hchsiao/openocd_jtag_dpi.git
-git clone http://vlsigit.ee.ncku.edu.tw/hchsiao/zero-buggy.git
+git clone https://github.com/hchsiao/zero-buggy.git
 
 cd $ROOT/riscv-fesvr
 mkdir -p build
